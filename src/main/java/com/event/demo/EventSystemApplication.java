001d0762e130e6e298fc299b2b3b315ad5f59da5
Jackey <jackey31416@gmail.com>
2021-01-18 15:38:22 +0800

    01181500

---
+++ src/main/java/com/event/demo/EventSystemApplication.java
@@ -0,0 +1,13 @@
+package com.event.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventSystemApplication.class, args);
+	}
+
+}
